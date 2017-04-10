a64412d4293f8c9e44fad847ac30c90676862f44
CelicaMo <xiaopengmo@163.com>
2017-04-10 11:04:03 +0800

    file upload

---
+++ repositoryfile.sql
@@ -0,0 +1,20 @@
+﻿INSERT INTO "repositoryfile" VALUES ('08e5856c-6fe6-43ad-bb1c-5b3ffb837581', '1c72f338-a47e-4b58-9c27-2ccdcce9377f', 'D:\Code\新闻.txt', '新闻.txt', '0.00', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO "repositoryfile" VALUES ('1092e37b-e30c-4b14-8f98-e645628d1a7d', '1c72f338-a47e-4b58-9c27-2ccdcce9377f', 'D:\CodeGet\5_2_0.zip', '5_2_0.zip', '29.20', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO "repositoryfile" VALUES ('24905', '1c72f338-a47e-4b58-9c27-2ccdcce9377f', 'D:\Code\新闻.txt', '新闻.txt', '0.00', NULL, NULL, 'f', NULL, NULL, NULL, '0001-1-1', '00:,0:0,.00', NULL);
+INSERT INTO "repositoryfile" VALUES ('3a9ad3b9-f2bb-41d4-b266-240eec0431c3', '1c72f338-a47e-4b58-9c27-2ccdcce9377f', 'D:\Code\新闻.txt', '新闻.txt', '0.00', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO "repositoryfile" VALUES ('3c15698d-4e96-49ad-b758-a4657f49a6a3', '1c72f338-a47e-4b58-9c27-2ccdcce9377f', 'D:\Code\新闻.txt', '新闻.txt', '0.00', NULL, NULL, 'f', '2017-3-21', '17:,3:7,.47', 'admin', '2017-3-21', '17:,3:7,.47', 'admin');
+INSERT INTO "repositoryfile" VALUES ('450900fc-03d8-4a23-bf2a-92a6b343d7bd', '1c72f338-a47e-4b58-9c27-2ccdcce9377f', 'D:\CodeGet\5_2_0.zip', '5_2_0.zip', '29.20', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO "repositoryfile" VALUES ('53531174-cd10-44ec-a71c-6922b57060c8', '1c72f338-a47e-4b58-9c27-2ccdcce9377f', 'D:\CodeGet\5_2_0.zip', '5_2_0.zip', '29.20', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO "repositoryfile" VALUES ('94cc048c-ad1c-489c-a43d-7a8bc32ddd58', '1c72f338-a47e-4b58-9c27-2ccdcce9377f', 'D:\Code\新闻.txt', '新闻.txt', '0.00', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO "repositoryfile" VALUES ('c8842fe4-3c9e-4431-be10-50c3574787c8', '1c72f338-a47e-4b58-9c27-2ccdcce9377f', 'D:\Code\5.2.rar', '5.2.rar', '29.20', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO "repositoryfile" VALUES ('ca1ffa92-9fac-4b4c-8482-b3189a23dbdb', '1c72f338-a47e-4b58-9c27-2ccdcce9377f', 'D:\CodeGet\5_2_0.zip', '5_2_0.zip', '29.20', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO "repositoryfile" VALUES ('d02ecaf9-1fa0-4241-bb16-fc17050552b1', '1c72f338-a47e-4b58-9c27-2ccdcce9377f', 'D:\CodeGet\5_2_0.zip', '5_2_0.zip', '29.20', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO "repositoryfile" VALUES ('e0181362-e73a-4b7c-bab0-95bfb575e1d4', '1c72f338-a47e-4b58-9c27-2ccdcce9377f', 'D:\Code\5_2_0.zip', '5_2_0.zip', '29.20', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO "repositoryfile" VALUES ('e36233ea-b29c-421f-95de-1e2862c40f56', '1c72f338-a47e-4b58-9c27-2ccdcce9377f', 'D:\CodeGet\5_2_0.zip', '5_2_0.zip', '29.20', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO "repositoryfile" VALUES ('effa2b75-7539-4316-96b3-6a2f4731a3b7', '1c72f338-a47e-4b58-9c27-2ccdcce9377f', 'D:\CodeGet\5_2_0.zip', '5_2_0.zip', '29.20', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO "repositoryfile" VALUES ('f31636f6-a3ad-4a2f-92ec-5856eccf36b9', '1c72f338-a47e-4b58-9c27-2ccdcce9377f', 'D:\Code\新闻.txt', '新闻.txt', '0.00', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO "repositoryfile" VALUES ('fd91acfa-4819-4e07-a363-0b77bebe1cdd', '1c72f338-a47e-4b58-9c27-2ccdcce9377f', 'D:\Code\新闻.txt', '新闻.txt', '0.00', NULL, '493d4c4bb9b29f8bcdede2f6d8aec31c94b03078', 'f', '2017-3-21', '17:,4:2,.45', 'admin', '2017-3-21', '17:,4:2,.45', 'admin');
+
+
+
+AAAAA
